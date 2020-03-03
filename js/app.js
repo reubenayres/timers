b6bbf24e4ad79da21778e127e073822aa10eb668
Reuben Ayres <ra@sent.com>
2020-03-03 13:02:01 -0600

    it counts up in the console from 0

--- js/app.js
+++ js/app.js
@@ -2,3 +2,10 @@ console.log("timers and stopwatches");
 
 // using setInterval make it so that you see a count starting at zero 
 // in your console and printing the next integer every second
+
+let timeElapsed = 0
+console.log(timeElapsed);
+setInterval(() => {
+  timeElapsed++
+  console.log(timeElapsed);
+}, 1000)
