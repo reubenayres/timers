f5ed76afed0867bc3a9e59ba20ccc7298f44c782
Reuben Ayres <ra@sent.com>
2020-03-03 13:05:38 -0600

    prompt -- make it stop at 10 using clearInterval()

--- js/app.js
+++ js/app.js
@@ -3,6 +3,9 @@ console.log("timers and stopwatches");
 // using setInterval make it so that you see a count starting at zero 
 // in your console and printing the next integer every second
 
+// research clearInterval and make it so that the counter counts to 
+// 10 and then stops
+
 let timeElapsed = 0
 console.log(timeElapsed);
 setInterval(() => {
